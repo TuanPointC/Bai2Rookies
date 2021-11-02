5c65a5bf18092a5ed5a9a2900dfc5aa96bc51914
TuanPointC <tuan.nv19010040@st.phenikaa-uni.edu.vn>
2021-11-02 20:20:56 +0700

    done post

--- src/Posts/PostDetail.js
+++ src/Posts/PostDetail.js
@@ -1,10 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Spin, Table } from 'antd'
+import axios from 'axios'
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+    },
+    {
+        title: 'Title',
+        dataIndex: 'title',
+    },
+    {
+        title: 'Body',
+        dataIndex: 'body',
+    },
+];
+const PostDetail = () => {
+    const { id } = useParams();
+    const [post, setPost] = useState(null)
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res =>{
+                setPost(res.data)
+            })
+                
+    },[id])
+    if (post === null) {
 
-const PostDetail =()=>{
-
+        return <Spin size="large" style={{ position: 'absolute', top: '50%', left: '50%', translate: '-50%,-50%' }} />
+    }
     return (
-        <div>
-            Post detail {this.props.match.params.id}
-        </div>
+        <Table columns={columns} dataSource={[post]} bordered style={{margin:'20px 0'}} pagination={false}/>
     )
 }
 
--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,16 +1,16 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react';
-import {useRouteMatch,Link,Switch} from 'react-router-dom'
-import { Table, Space,Button ,Input,Spin} from 'antd';
+import { useRouteMatch, Link } from 'react-router-dom'
+import { Table, Space, Button, Input, Spin } from 'antd';
 const { Column } = Table;
 const { Search } = Input;
 
 
 const Posts = () => {
     const [listPosts, setListPosts] = useState([])
-    const [listPostsCopy,setListPostsCopy] =useState([])
-    const [rowWasEntered,setRowWasEntered] = useState(null)
-    let { path, url } = useRouteMatch();
+    const [listPostsCopy, setListPostsCopy] = useState([])
+    const [rowWasEntered, setRowWasEntered] = useState(null)
+    let {  url } = useRouteMatch();
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -20,56 +20,57 @@ const Posts = () => {
             })
     }, [])
 
-    const search =(value)=>{
-        var listP = listPosts.filter(p=>p.title.indexOf(value)>-1)
+    const search = (value) => {
+        var listP = listPosts.filter(p => p.title.indexOf(value) > -1)
         setListPostsCopy([...listP])
-    
+
     }
 
-    const remove =()=>{
+    const remove = () => {
         //delete listCopy
-        var index = listPostsCopy.findIndex(p=>p.id===rowWasEntered)
+        var index = listPostsCopy.findIndex(p => p.id === rowWasEntered)
         var newList = listPostsCopy
-        newList.splice(index,1)
+        newList.splice(index, 1)
         setListPostsCopy([...newList])
 
         //delete original list
-        var index1 = listPosts.findIndex(p=>p.id===rowWasEntered)
+        var index1 = listPosts.findIndex(p => p.id === rowWasEntered)
         var newList1 = listPosts
-        newList.splice(index1,1)
+        newList.splice(index1, 1)
         setListPosts([...newList1])
 
         //set rowWasEnterd increase 1
-        setRowWasEntered(prev=>prev+1)
+        setRowWasEntered(prev => prev + 1)
 
     }
-    if(listPosts.length===0){
-        return  <Spin size="large" style={{ position:'absolute',top:'50%',left:'50%',translate:'-50%,-50%' }}/>
+    if (listPosts.length === 0) {
+        return <Spin size="large" style={{ position: 'absolute', top: '50%', left: '50%', translate: '-50%,-50%' }} />
     }
     return (
         <div>
-            <Search placeholder="input search text"  enterButton style={{width:'200px', margin:'20px 0'}} onSearch={(event)=>search(event)}/>
-            <Table 
-                dataSource={listPostsCopy} 
-                bordered 
+            <Search placeholder="input search text" enterButton style={{ width: '200px', margin: '20px 0' }} onSearch={(event) => search(event)} />
+            <Table
+                dataSource={listPostsCopy}
+                bordered
                 onRow={(record) => {
                     return {
-                      onMouseEnter: () => {setRowWasEntered(record.id)}, // mouse enter row
-                     
-                    }}}
+                        onMouseEnter: () => { setRowWasEntered(record.id) }, // mouse enter row
+
+                    }
+                }}
             >
 
-                <Column title="Id" dataIndex="id" key={Math.random()}  />
-                <Column title="Title" dataIndex="title" key={Math.random()} size="large" sorter={(a, b) => a.title.localeCompare(b.title) }/>
-                <Column 
+                <Column title="Id" dataIndex="id" key={Math.random()} />
+                <Column title="Title" dataIndex="title" key={Math.random()} size="large" sorter={(a, b) => a.title.localeCompare(b.title)} />
+                <Column
                     title="Actions"
-                    align= 'right'
+                    align='right'
                     key={Math.random()}
                     size="small"
                     render={() => (
                         <Space size="large">
-                             <Button type="primary"><Link to={`${url}/post`}>View detail</Link></Button>
-                            <Button type="primary" danger onClick={()=>remove("id")}>Remove</Button>
+                            <Button type="primary"><Link to={`${url}/${rowWasEntered}`}>View detail</Link></Button>
+                            <Button type="primary" danger onClick={() => remove("id")}>Remove</Button>
                         </Space>
                     )}
                 />
